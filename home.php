99a5f620302c195a43472c8fec45dc629a8de76f
jiosaa <batisileshi93@gmail.com>
2023-01-10 15:00:43 -0500

    FOW started

---
+++ home.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+
+
+<?php include ('templates/header_home.php') ?>
+
+<h4>Order my food dashboard</h4>
+
+<?php include ('templates/footer_home.php') ?>
+
+
+</html>
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+
+<?php include ('templates/header.php') ?>
+
+<h4>Order my food </h4>
+
+
+<?php include ('templates/footer.php') ?>
+
+
+</html>
