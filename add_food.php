b9b05eeccb35b04e1cfa2e40413ebea08c8c838c
jiosaa <batisileshi93@gmail.com>
2023-01-11 01:44:56 -0500

    templates added to index

--- add_food.php
+++ add_food.php
@@ -4,8 +4,10 @@
 if (isset($_POST['submit']) && isset($_FILES['image'])) {
 	include ('db_connection/db_connection.php');
 
-    $name = $_POST['name'];
-    $price = $_POST['price'];
+
+	$name = mysqli_real_escape_string($connect, $_POST['name']);
+    $price = mysqli_real_escape_string($connect, $_POST['price']);
+	$description = mysqli_real_escape_string($connect, $_POST['description']);
 
 	$img_name = $_FILES['image']['name'];
 	$img_size = $_FILES['image']['size'];
@@ -28,8 +30,8 @@ if (isset($_POST['submit']) && isset($_FILES['image'])) {
 				move_uploaded_file($tmp_name, $img_upload_path);
 
 				// Insert into Database
-				$sql = "INSERT INTO foods(name, price, image_url) 
-				        VALUES('$name', '$price', '$new_img_name')";
+				$sql = "INSERT INTO foods(name, price, description, image_url) 
+				        VALUES('$name', '$price', '$description', '$new_img_name')";
 				mysqli_query($connect, $sql);
 				header("Location: home.php");
 			}else {
--- index.php
+++ index.php
@@ -1,13 +1,105 @@
 <!DOCTYPE html>
 <html>
 
+<?php include ('db_connection/db_connection.php'); 
+
+
+$sql = "SELECT * FROM foods ORDER BY id DESC ";
+$result = mysqli_query($connect,  $sql);
+$foods = mysqli_fetch_all($result, MYSQLI_ASSOC)
+
+
+?>
 
 <?php include ('templates/header.php') ?>
 
-<h4>Order my food </h4>
+
+    <section class="food_section layout_padding-bottom">
+    <div class="container">
+      <div class="heading_container heading_center">
+        <h2>
+          Our Menu
+        </h2>
+      </div>
+
+      
+
+      <div class="filters-content">
+        <div class="row grid">
+
+        <?php foreach($foods as $food){ ?>
+
+          <div class="col-sm-6 col-lg-4 all pizza">
+            <div class="box">
+              <div>
+                <div class="img-box">
+                  <img src="uploads/<?=$food['image_url']?>" alt="Food image">
+                </div>
+                <div class="detail-box">
+                  <h5>
+                  <?php echo htmlspecialchars($food['name']); ?>
+                  </h5>
+                  <p>
+                  <?php echo htmlspecialchars($food['description']); ?>
+                  </p>
+                  <div class="options">
+                    <h6>
+                    <?php echo htmlspecialchars($food['price']); ?> Birr
+                    </h6>
+                  
+                      <a href="#" 
+                      data-name="<?php echo htmlspecialchars($food['name']); ?>" 
+                      data-price="<?php echo htmlspecialchars($food['price']); ?>" 
+                      class="add-to-cart btn btn-primary"> <i class="fa fa-cart-plus " aria-hidden="true"></i></a>
+                  
+                    
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <?php } ?>
+
+
+        </div>
+      </div>
+      <div class="btn-box">
+        <a href="">
+          View More
+        </a>
+      </div>
+    </div>
+   </section>
 
 
 
+   
+   <div class="modal fade" id="cart" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Cart</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <table class="show-cart table">
+            
+          </table>
+          <div>Total price:<span class=" ml-1 mr-1 total-cart"></span> <em>Birr</em></div>
+        </div>
+       
+        <div class="modal-footer">
+          <button class="clear-cart btn btn-danger">Clear Cart</button>
+          <a href="order.html"> <button type="button" class="btn btn-primary" >Order now</button></a>
+         
+        </div>
+      </div>
+    </div>
+   </div> 
+  
 
 <?php include ('templates/footer.php') ?>
 
